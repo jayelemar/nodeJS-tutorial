8f44df95b37744457076f8516aa93ec64e33be17
Jay Termulo <jetermulo@gmail.com>
2023-09-07 13:43:16 +0800

    Create Task, Save and get task from database

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,39 @@
 const dotenv = require("dotenv").config()
 const express = require("express");
 const connectDB = require('./config/connectDB')
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const Task = require("./model/taskModel");
+
 
 const app = express() 
 
-app.get("/", (req, res) => {
-    res.send("Home page");
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+//Routes
+app.get("/", (request, response) => {
+    response.send("Home page");
 })
 
-// Create a Task
+// Create / Post a Task
 app.post("/api/tasks", async (request, response) => {
-    console.log(request.body);
-    response.send("Task Created")
+    try {
+        const task = await Task.create(request.body);
+        response.status(200).json(task);
+    } catch (error) {
+        response.status(500).json({msg: error.message})
+    }
+})
+
+// Get / Read Data
+app.get("/api/tasks", async(request, response) => {
+    try {
+        const tasks = await Task.find() // get all data , if find(id) get only on that specific id
+        response.status(200).json(tasks)
+    } catch (error) {
+        response.status(500).json({ msg: error.message })
+    }
 })
 
 const PORT  = process.env.PORT || 5000
